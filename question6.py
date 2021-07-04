9c4b7b3e4ab41367089f151a259c9365a77a1a32
KrushnaEWaghmare <waghmarekrushna07@gmail.com>
2021-07-04 09:09:29 +0530

    Create question6.py

---
+++ question6.py
@@ -0,0 +1,11 @@
+# cook your dish here
+T=int(input())                                  #input tests
+for i in range(T):                              
+    c,d,l=map(int,input().split())
+    if l%4==0:                                  #condition for 4 legs
+        if l<=4*(c+d) and l>=4*max(c-d,d):      #it check condition for leg 4 and leg 8
+            print('yes')                        #print yes if condition satisfied
+        else:
+            print('no')                         #else print no
+    else:
+        print('no')
