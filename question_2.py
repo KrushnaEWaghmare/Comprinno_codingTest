e655bd37382b720bb59111d356717f1a806dff5a
KrushnaEWaghmare <waghmarekrushna07@gmail.com>
2021-07-04 10:02:58 +0530

    Update question_2.py

--- question_2.py
+++ question_2.py
@@ -1,3 +1,10 @@
+#1.	Take number of test as input T.
+#2.	Perform all validity check on T (1 ≤ T ≤ 1000)
+#3.	Create a blank list
+#4.	Append values of a,b.c
+#5.	Assign the index values to a,b,c
+#6.	Check the condition a[0]<=b[0]<=c[0] and a[1]<=b[1]<=c[1] and a[2]<=b[2]<=c[2]:if true then return yes else return no
+#7.	Store the splited  each element I in ans[]
 
 def solve(a,b,c):
     if a == b or a == c or b == c:      
