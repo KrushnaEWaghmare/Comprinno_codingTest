a2281c22f14c3f1b7a5be04a628373067cbd16e2
KrushnaEWaghmare <waghmarekrushna07@gmail.com>
2021-07-04 10:03:44 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,3 +1,15 @@
+#1.	Take number of test as input T.
+#2.	Perform all validity check on T (1 ≤ T ≤ 50 1 ≤ N ≤ 50)
+#3.	Split the input by using split()
+#4.	Set a variable c=0 which check the count
+#5.	Check condition for cookie if yes then increment the count of c by 1 else set it as 0
+#6.	Check the count of cookie is equal to 2 then break the loop by using Break
+#7.	And last check value of c==0 then print Yes otherwise print No
+
+
+
+
+
 t=int(input())            #take input t
 for k in range(t):        
     n = int(input())      #stores the input in n.
