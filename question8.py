cf2a8e04df825706815dc54ee1d2c17a6ae9ed6d
KrushnaEWaghmare <waghmarekrushna07@gmail.com>
2021-07-04 10:19:16 +0530

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,3 +1,9 @@
+#1.number of test cases
+#2.split and apply int to to every element
+#3.starting turn valute is 0 and every increment the value increments and and multiple the value of 2 times of number
+#4.(a = 2, b = 2) in first turn and max(a, b)/min(a, b) = 2/2 print ans that is 1.
+
+
 d = int(input())                              #input accept
 for i in range(d):
     t = [int(i) for i in input().split()]     #applies int to each string element in the input
