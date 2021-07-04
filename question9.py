c8131a218bae77ac0a219907672e4c2bf0ca8c0b
KrushnaEWaghmare <waghmarekrushna07@gmail.com>
2021-07-04 10:09:06 +0530

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,3 +1,9 @@
+#1.Accept the input
+#2.Apply int to each string element in the input\
+#3.store the input in list
+#4.sort the list by using inbuild sort()
+#5.print the min value by using the min() and decrement the array size by 1
+
 for i in range(int(input())):                     #accept inputs
     ele=int(input())                               #store it in ele
     l=map(int,input().split())                    
